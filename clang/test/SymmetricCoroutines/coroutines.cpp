bcae138b4386fa2e56875a9ad2653a49b78c6198
Mihail Mihaylov <mmihailov@vmware.com>
2019-08-12 05:41:12 -0700

    I added a small number of unit tests.

---
+++ clang/test/SymmetricCoroutines/coroutines.cpp
@@ -0,0 +1,50 @@
+// RUN: %clang_cc1 %s -verify
+// XFAIL: *
+
+// Positive tests
+
+// expected-no-diagnostics
+
+#include <__symmetric_coroutine>
+
+using std::coroutine;
+using std::resume_continuation;
+
+// Coroutine with no creation parameters and an empty body
+minimal() : coroutine<void(void)> 
+{}
+
+void use_minimal() {
+   minimal coro;
+   coro();
+}
+
+// Coroutine with one creation parameter passed by value and an empty body
+one_value_param_empty(int a) : coroutine<void(void)> {
+}
+
+void use_one_value_param_empty() {
+   int a = 0;
+   one_value_param_empty coro{a};
+   coro();
+}
+
+// Coroutine with one creation parameter passed by reference and an empty body
+one_ref_param_empty(int& a) : coroutine<void(void)> {
+}
+
+void use_one_ref_param_empty(int& a) {
+   int a = 0;
+   one_ref_param_empty coro{a};
+   coro();
+}
+
+// Coroutine with one creation parameter passed by r-value reference and an empty body
+one_rv_ref_param_empty(int&& a) : coroutine<void(void)> {
+}
+
+void use_one_rv_ref_param_empty() {
+   int a;
+   one_rv_ref_param_empty coro{a};
+   coro();
+}
---
+++ clang/test/SymmetricCoroutines/include_coroutines_header.cpp
@@ -0,0 +1,5 @@
+// RUN: %clang_cc1 %s -verify
+// XFAIL: *
+// expected-no-diagnostics
+
+#include <__symmetric_coroutine>
